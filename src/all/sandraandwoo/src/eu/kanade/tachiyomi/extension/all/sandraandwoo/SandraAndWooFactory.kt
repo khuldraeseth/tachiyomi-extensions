965490e9031d57683c28b430dd94d482556e463c
Khuldraeseth <khuldraeseth@gmail.com>
2022-08-10 20:59:29 -0700

    Add Sandra and Woo source

---
+++ src/all/sandraandwoo/src/eu/kanade/tachiyomi/extension/all/sandraandwoo/SandraAndWooFactory.kt
@@ -0,0 +1,11 @@
+package eu.kanade.tachiyomi.extension.all.sandraandwoo
+
+import eu.kanade.tachiyomi.extension.all.sandraandwoo.translations.SandraAndWooDE
+import eu.kanade.tachiyomi.source.SourceFactory
+
+class SandraAndWooFactory : SourceFactory {
+    override fun createSources() = listOf(
+        SandraAndWoo(),
+        SandraAndWooDE(),
+    )
+}
---
+++ src/all/sandraandwoo/src/eu/kanade/tachiyomi/extension/all/sandraandwoo/translations/SandraAndWooDE.kt
@@ -0,0 +1,10 @@
+package eu.kanade.tachiyomi.extension.all.sandraandwoo.translations
+
+import eu.kanade.tachiyomi.extension.all.sandraandwoo.SandraAndWoo
+
+class SandraAndWooDE : SandraAndWoo("http://www.sandraandwoo.com", "de") {
+    override val name = "Sandra und Woo"
+
+    override val synopsis = "Sandra und Woo ist ein Comedy-Webcomic mit dem zwölfjährigen Mädchen Sandra North und ihrem Waschbären Woo in den Hauptrollen. Zwar sollen die meisten Strips einfach nur lustig oder gewitzt sein, gelegentlich werden aber auch ernste Themen wie die Missachtung von Menschenrechten in Burma oder die Zerstörung der Natur angesprochen. Wir wollen außerdem zeigen, was es für Sandra und ihre Schulfreunde Cloud und Larisa bedeutet, erwachsen zu werden. Nicht vergessen werden sollten außerdem Woos Ausflüge in den nahen Wald um dort seine tierischen Freunde Shadow, ein Fuchs, und Sid, ein Eichhörnchen, zu treffen. Als weiteres Alleinstellungsmerkmal des Comics darf gelten, dass sich immer wieder einzelne Strips oder sogar längere Geschichten um die Nebenfiguren drehen."
+    override val archive = "/woode/archiv"
+}
